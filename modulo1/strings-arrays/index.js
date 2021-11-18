abd341e8e6971fc38f961b8bfce61a936335b220
AlexBitenc0urt <alexx.bittencourt@gmail.com>
2021-11-17 22:28:16 -0300

    add exercicio de strings e arrays

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,63 @@
+// let array
+// console.log('a. ', array)
+
+// array = null
+// console.log('b. ', array)
+
+// array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// console.log('c. ', array.length)
+
+// let i = 0
+// console.log('d. ', array[i])
+
+// array[i+1] = 19
+// console.log('e. ', array)
+
+// const valor = array[i+6]
+// console.log('f. ', valor)
+
+// const frase = prompt("Digite uma frase")
+
+// console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+
+// let email = prompt("Digite seu email.")
+// let nome = prompt("Digite seu nome.")
+
+// console.log(`O e-mail ${email} foi cadastrado com sucesso. Seja bem-vinda(o), ${nome}!`);
+
+// const comida1 = "feijoada"
+// const comida2 = "quiabada" 
+// const comida3 = "mocoto" 
+// const comida4 = "cozido" 
+// const comida5 = "buchada de bode"
+// const comidas = [comida1, comida2, comida3, comida4, comida5]
+
+// console.log(`Essas são as minhas comidas preferidas:`);
+// console.log(comidas[0]);
+// console.log(comidas[1]);
+// console.log(comidas[2]);
+// console.log(comidas[3]);
+// console.log(comidas[4]);
+
+// comidas[1] = prompt("Qual sua comida favorita?")
+
+// console.log(comidas);
+
+// listadeTarefas = []
+// listadeTarefas [0]= prompt("Digite sua primeira tarefa")
+// listadeTarefas [1]= prompt("Digite a segunda")
+// listadeTarefas [2]= prompt("Digite a terceira")
+
+//  console.log(listadeTarefas);
+
+// let indice = Number( prompt(`Digite a tarefa realizada, digite 0, 1 ou 2`))
+
+//  listadeTarefas.splice(indice, 1)
+
+//  console.log(listadeTarefas);
+
+
+
+
+
+
