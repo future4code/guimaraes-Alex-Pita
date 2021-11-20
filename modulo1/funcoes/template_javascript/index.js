2de28ee358d9bed23c7fd4c358ae60aac28f3d69
AlexBitenc0urt <alexx.bittencourt@gmail.com>
2021-11-19 23:05:01 -0300

    add exercício de funcão

---
+++ modulo1/funcoes/template_javascript/index.js
@@ -0,0 +1,83 @@
+//1.a 10 e 50
+//1.b não iria imprimir nada.
+//2.a O método includes() determina se um array/objeto contém um determinado elemento ou não, retornando true ou false,
+//2.b retorna true nas três perguntas, pois as três contem a palavra chave.
+
+// function meusDados1(){
+
+// }
+
+// console.log("Eu sou Alex, tenho 47 anos, moro em Itaparica e sou estudante")
+
+//  function meusDados2(nome, idade, cidade, profissao){
+
+//     console.log(`Eu sou ${nome} tenho ${idade} anos, moro em ${cidade} e sou ${profissao}.`);
+
+//  }
+
+//  meusDados2("Alex", 37, "Itaparica", "Fiscal")
+
+// function soma(numero1, numero2){
+// let somar = numero1 + numero2
+
+// return somar
+
+// }
+
+// console.log(soma(10, 15));
+
+// function comparar(numero1, numero2){
+
+// let verificar = numero1 >= numero2
+
+// return verificar
+
+// }
+
+// function parouNao(numero1){
+//     let operacao = numero1 % 2 === 0
+
+//     return operacao
+// }
+
+// function texto(string){
+
+//     let tamanho = string.length
+//     let maiusculas = string.toUpperCase()
+
+//     let unir = tamanho + " " + maiusculas
+
+//     return unir
+
+// }
+
+// console.log(texto("Alex"));
+
+// let somaNum = (num1, num2) => {
+
+//     return num1 + num2
+// }
+
+// let subNum = (num1, num2) => {
+
+//     return num1 - num2
+// }
+
+// let multiNum = (num1, num2) => {
+
+//     return num1 * num2
+// }
+
+// let divNum = (num1, num2) => {
+
+//     return num1 / num2
+// }
+
+// let num1 = Number( prompt("Qual o seu primeiro número?"))
+// let num2 = Number( prompt("Qual o seu segundo número?"))
+
+// console.log(`Números inseridos: ${num1} e ${num2}`);
+// console.log("Soma:", somaNum(num1, num2));
+// console.log("Diferença:", subNum(num1, num2));
+// console.log("Multiplicação:", multiNum(num1, num2));
+// console.log("Divisão:", divNum(num1, num2));
