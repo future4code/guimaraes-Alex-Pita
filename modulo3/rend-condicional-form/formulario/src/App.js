255f95cd77261e609bb62eeadd415ed2e239a6a9
AlexBitenc0urt <alexx.bittencourt@gmail.com>
2022-02-10 15:31:58 -0300

    complemento do exercicio

--- modulo3/rend-condicional-form/formulario/src/App.js
+++ modulo3/rend-condicional-form/formulario/src/App.js
@@ -1,33 +1,93 @@
-import { render } from '@testing-library/react';
-import './App.css';
-import Etapa1 from './components/Etapa1';
-import Etapa2 from './components/Etapa2';
-import Etapa3 from './components/Etapa3';
-import Final from './components/Final';
+import React from 'react';
+import * as S from './styles';
 
-export default function App() {
- state = {
-   login: false
+import Question from '../src/components/Question/';
 
- }
-
-  render () {
-    let pagina = <h1>Página erro</h1>
-    if (this.state.login) {
-      pagina = <Etapa1 />
-    } else {
-      pagina = <Etapa2 />
-    }
+class App extends React.Component {
+  state = {
+    nextEtapa: 0
   }
 
-  return (
-    <div className="App">
 
-      <Etapa1 />
-      <Etapa2 />
-      <Etapa3 />
-      <Final />
- 
-    </div>
-  );
+  handleNextEtapa = () => {
+    this.setState({ nextEtapa: this.state.nextEtapa + 1 });
+    console.log("State: ", this.state.nextEtapa)
+  }
+
+  render(){
+
+    if(this.state.nextEtapa === 0){
+      return (
+      <S.Container>
+            
+            <S.Form>
+              <Question
+                etapa={'ETAPA 1 - DADOS GERAIS'}
+                question={'Qual o seu nome?'}
+              />
+
+              <Question
+                question={'2. Qual sua Idade?'}
+              />
+
+              <Question
+                question={'3. Qual seu e-mail?'}
+              />
+
+              <Question
+                question={'4. Qual a sua escolarida?'}
+                type={'list'}
+              />   
+              <button onClick={this.handleNextEtapa}>Próxima etapa</button>                  
+          </S.Form> 
+              </S.Container>
+      )
+    }else{
+    if(this.state.nextEtapa === 1){
+       return (<S.Container>
+        <S.Form>
+            <Question
+              etapa={'ETAPA 2 - INFORMAÇÕES DO ENSINO SUPERIOR'}
+              question={'5. Qual curso?'}
+            />
+
+            <Question
+                question={'6. Qual a unidade de ensino?'}
+            />
+            <button onClick={this.handleNextEtapa}>Próxima etapa</button>  
+        </S.Form> 
+      </S.Container>)
+    }else{
+      if(this.state.nextEtapa === 2){
+         return(
+        <S.Container>
+        <S.Form>
+          <Question
+              etapa={'ETAPA 3 - INFORMAÇÕES GERAIS DE ENSINO'}
+              question={'5. Por que você não terminou um curso de graduação?'}
+             />
+
+              <Question
+                question={'6. Você fez algum curso complementar?'}
+                type={'curso'}
+              />  
+              <button onClick={this.handleNextEtapa}>Próxima etapa</button>  
+        </S.Form> 
+      </S.Container>
+      )
+      }else{
+        return(
+      <S.Container>
+        <S.Form>
+          <h1><b>O FORMULÁRIO ACABOU</b></h1>
+          <p>Muito obrigado por participar! Entraremos em contato!</p>
+        </S.Form> 
+      </S.Container>
+        )
+      }
+    }
+    }
+  }
 }
+
+export default App;
