68580e4863151cd5dff139fa97f4ad08100f7e17
AlexBitenc0urt <alexx.bittencourt@gmail.com>
2022-02-25 22:08:57 -0300

    adicionando parte do exercicio

--- modulo3/apis-labenusers/src/App.jsx
+++ modulo3/apis-labenusers/src/App.jsx
@@ -1,40 +1,46 @@
+import React from 'react';
 import './App.css';
 import axios from 'axios'
 
-Class App extends React.Component {
-  return (
-    <div className="App">
-       <h1>Olá</h1>
-    
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    listaUser: []
+  }
+
+  listarTodosUser = () =>{
+    axios
+      .get('https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users', 
+        {
+          headers:{
+             Authorization: 'alex-bitencourt-guimaraes'
+          } 
+        }  
+      )
+      .then((res) => {
+        this.setState({listaUser: res.data.list})
+        console.log(res.data.list);
+      })
+      .catch((err) => console.log(err))
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+        <button onClick={this.listarTodosUser}>Trocar de Tela</button>
+        </div>
+         <input placeholder="Nome"/>
+         <input placeholder="E-mail"/>
+         <button onClick={this.listarTodosUser}>Criar Usuário</button>
+               
+      </div>
+    );
+  }
+
 }
 
 
-
-
-const urlUserslist = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users"
-
-const headers = {
-    headers:{
-      Authorization: "alex-bitencourt-guimaraes"
-    }
-
-  
-}
-
-const getAllUsers = () =>{
-
-  axios
-  .get(urlUserslist,headers)
-  .then((response)=>{
-    console.log(response.data.result.list);
-  })
-  .catch((error) =>{
-    console.log(error.response);
-  })
-}
-
-getAllUsers()
 
 export default App;
