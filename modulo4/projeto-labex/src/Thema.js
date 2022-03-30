d8562ea52554faf7107ffead58a2874592c5f93d
AlexBitenc0urt <alexx.bittencourt@gmail.com>
2022-03-29 22:10:04 -0300

    add projeto LabeX

---
+++ modulo4/projeto-labex/src/Thema.js
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react"
+
+export const theme = extendTheme({
+    colors: {
+      brand: {
+        100: "ff0000",
+        900: "#FC8181",
+      },
+    },
+  })
