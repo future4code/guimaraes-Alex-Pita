c6d7e97fd4d63ec0b12c26a9e837f71c951b0d13
AlexBitenc0urt <alexx.bittencourt@gmail.com>
2022-08-02 22:02:37 -0300

    add atualizações

--- src/error/ShortName.ts
+++ src/error/ShortName.ts
@@ -2,6 +2,6 @@ import { CustomError } from "./CustomError";
 
 export class ShortName extends CustomError{
     constructor(){
-        super("O nome deve ser maior ",400)
+        super("O nome deve ser maior que 10 caracteres",400)
     }
 }
